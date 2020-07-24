9511aca7dd32139352f4cc42997a2a721f7eb5ea
t.takahashi <takahashi.developer@gmail.com>
2020-07-24 12:31:08 +0900

    l-tika by c

---
+++ c/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <wiringPi.h>
+
+#define LED_PIN 4
+
+int end = 0;
+
+void abort_handler(int signal);
+
+int main(void) {
+  wiringPiSetup();
+  pinMode(LED_PIN, OUTPUT);
+
+  if (signal(SIGINT, abort_handler) == SIG_ERR) {
+    exit(1);
+  }
+
+  while (!end) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);
+  }
+
+  pinMode(LED_PIN, INPUT);
+
+  return 0;
+}
+
+void abort_handler(int signal) {
+  end = 1;
+}
+
